e76b46a8e2b610cae56b02f8d051a3e19b9ff65f
FallenHell <gabriel.ferreri98@gmail.com>
2020-08-17 21:00:04 -0500

    inserindo projeto completo no git para demonstracao

---
+++ Models/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace CasaDoCodigo.Models
+{
+
+    [DataContract]
+    public class BaseModel
+    {
+        [DataMember]
+        public int Id { get; protected set; }
+    }
+
+}
